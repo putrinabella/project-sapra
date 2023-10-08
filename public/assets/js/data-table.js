17fbd57b81801bf9a69248e3cc2e81d15c63fb46
putrinabella <2011016220026@mhs.ulm.ac.id>
2023-10-09 07:50:38 +0800

    Fix the datatable search issue

--- public/assets/js/data-table.js
+++ public/assets/js/data-table.js
@@ -9,7 +9,7 @@ $(function() {
       ],
       "iDisplayLength": 10,
       "search": {
-        "smart": true
+        "smart": false
       },
       "language": {
         search: ""
@@ -17,11 +17,9 @@ $(function() {
     });
     $('#dataTable').each(function() {
       var datatable = $(this);
-      // SEARCH - Add the placeholder for Search and Turn this into in-line form control
       var search_input = datatable.closest('.dataTables_wrapper').find('div[id$=_filter] input');
       search_input.attr('placeholder', 'Search');
       search_input.removeClass('form-control-sm');
-      // LENGTH - Inline-Form control
       var length_sel = datatable.closest('.dataTables_wrapper').find('div[id$=_length] select');
       length_sel.removeClass('form-control-sm');
     });
